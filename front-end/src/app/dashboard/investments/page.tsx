d823088fcb220d0db3bd62bc58330c96c673adeb
lucasccgomes <lucasccgomes@protonmail.com>
2024-11-22 06:51:45 -0300

    feat(investments): add investments management screen
    - Created an investments screen with a welcoming title and description.
    - Styled the layout with a centered design for user engagement.
    - Placeholder content added to inform users about the functionality.

---
+++ front-end/src/app/dashboard/investments/page.tsx
@@ -0,0 +1,9 @@
+export default function Investments() {
+    return (
+      <div className="flex flex-col items-center justify-center min-h-screen gap-4">
+        <h1 className="text-2xl font-bold">Área de Investimentos</h1>
+        <p>Aqui você pode gerenciar seus investimentos!</p>
+      </div>
+    );
+  }
+  
